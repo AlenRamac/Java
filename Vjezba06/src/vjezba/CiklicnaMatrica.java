caca089e317549a4a505d69d4f8a2ceb080470b5
AlenRamac <112334120+AlenRamac@users.noreply.github.com>
2022-09-18 19:26:40 +0200

    Gotova ciklicna matrica

--- Vjezba06/src/vjezba/CiklicnaMatrica.java
+++ Vjezba06/src/vjezba/CiklicnaMatrica.java
@@ -1,10 +1,18 @@
 package vjezba;
 
+import javax.swing.JOptionPane;
+
 public class CiklicnaMatrica {
 
 	public static void main(String[] args) {
-//		proba(3, 3);
-		ciklicnaMatrica(6, 6);
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Upisi broj redaka"));
+		int y = Integer.parseInt(JOptionPane.showInputDialog("Upisi broj stupaca"));
+		if (x>0 || y>0){
+			ciklicnaMatrica(x, y);
+		}else {
+			System.out.println("Unesi brojeve vece od 0!");
+		}
+//		proba(4, 5);
 	}
 
 	private static void ciklicnaMatrica(int x, int y) {
@@ -13,7 +21,126 @@ public class CiklicnaMatrica {
 		int c1 = 0;
 		int c2 = 0;
 		int c3 = 0;
-		for (int b = 0; b < 4; b++) {
+		for (int b = 0; b < (x+y)/2; b++) {
+			
+			if (x<=2 || y<=2) {
+				if(b>0) {
+					break;
+				}
+			}
+//			desno
+			for (int j = 0+c2; j < polje[0].length-c1; j++) {
+				if (polje[0+c2][j] == 0 ) {
+					polje[0+c2][j] = a;
+					a += 1;
+				}
+			}
+			c2++;
+			if (x<=3) {
+				if(b>0) {
+					break;
+				}
+			}
+			if (x>4 && x%2 != 0) {
+				if(y>4 && y%2 == 0) {
+					if(b==(x/2)+2) {
+						break;
+					}
+				}
+			}
+			
+//			dolje
+			for (int i = 0+c2; i < polje.length-c1; i++) {
+				if (polje[i][polje[0].length-1-c1] == 0 ) {
+					polje[i][polje[0].length-1-c1] = a;
+					a += 1;
+				}
+			}
+			
+			if (x>4 && x%2 == 0) {
+				if(y>4 && y%2 != 0) {
+					if(b==(x/2)+2) {
+						break;
+					}
+				}
+			}
+			
+//			lijevo
+			for (int j = polje[0].length-1-c2; j > -1+c1; j--) {
+				if (polje[polje.length-1-c1][j] == 0 ) {
+					polje[polje.length-1-c1][j] = a;
+					a += 1;
+				}
+			}
+			c1++;
+			
+			if (x==4) {
+				if(b==(x/2)+1) {
+					break;
+				}
+			}
+			
+//			gore
+//			if ((float)x/2 != 0 && x>4) {
+//				if(b>x/2-2) {
+//					c3++;
+//				}
+//			}
+			if (x>4) {
+				if(b>0) {
+					c3++;
+				}
+			}
+			for (int i = polje.length-1-c2; i > -1+c1; i--) {
+				if (polje[i][0+c3] == 0 ) {
+					polje[i][0+c3] = a;
+					a += 1;
+				}
+			}
+			
+		}
+		
+		
+		
+//		print
+		for (int k = 0; k < polje.length; k++) {
+			for (int l = 0; l < polje[0].length; l++) {
+				//jednoznamenkasti
+				if (polje[k][l] < 10) {
+					System.out.print("   " + polje[k][l]);	
+				}
+				//dvoznamenkasti
+				if (polje[k][l] > 9 && polje[k][l] < 100) {
+					System.out.print("  " + polje[k][l]);	
+				}
+				//troznamekasti
+				if (polje[k][l] > 99 && polje[k][l] < 1000) {
+					System.out.print(" " + polje[k][l]);	
+				}
+			}
+			System.out.println();
+		}
+	
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static void proba(int x, int y) {
+		int[][] polje = new int [x][y];
+		int a = 1;
+		int c1 = 0;
+		int c2 = 0;
+		int c3 = 0;
+		for (int b = 0; b < x; b++) {
 			
 //			desno
 			for (int j = 0+c2; j < polje[0].length-c1; j++) {
@@ -79,67 +206,5 @@ public class CiklicnaMatrica {
 			System.out.println();
 		}
 	
-}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	private static void proba(int x, int y) {
-		int[][] polje = new int [x][y];
-		
-		int a = 1;
-		
-//		desno
-		for (int j = 0; j < polje[0].length; j++) {
-			polje[0][j] = a;
-			a += 1;
-		}
-//		dolje
-		for (int i = 1; i < polje.length; i++) {
-			polje[i][polje.length-1] = a;
-			a += 1;
-		}
-//		lijevo
-		for (int j = polje[0].length-2; j > -1; j--) {
-			polje[polje.length-1][j] = a;
-			a += 1;
-		}
-//		gore
-		for (int i = 1; i > 0; i--) {
-			polje[i][0] = a;
-			a += 1;
-		}
-//		desno
-		for (int j = 1; j < polje[0].length-1; j++) {
-			polje[1][j] = a;
-			a += 1;
-		}		
-		
-//		print
-		for (int k = 0; k < polje.length; k++) {
-			for (int l = 0; l < polje[0].length; l++) {
-				//jednoznamenkasti
-				if (polje[k][l] < 10) {
-					System.out.print("   " + polje[k][l]);	
-				}
-				//dvoznamenkasti
-				if (polje[k][l] > 9 && polje[k][l] < 100) {
-					System.out.print("  " + polje[k][l]);	
-				}
-				//troznamekasti
-				if (polje[k][l] > 99 && polje[k][l] < 1000) {
-					System.out.print(" " + polje[k][l]);	
-				}
-			}
-			System.out.println();
-		}
 	}
 }
