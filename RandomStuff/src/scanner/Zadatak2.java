1993de26558d8e967daec4680cb2bbfd14191833
AlenRamac <112334120+AlenRamac@users.noreply.github.com>
2023-04-08 10:38:49 +0200

    Create Zadatak2.java

---
+++ RandomStuff/src/scanner/Zadatak2.java
@@ -0,0 +1,14 @@
+package scanner;
+
+public class Zadatak2 {
+	
+	public static void main(String[] args) {
+		
+		byte dan = 11;
+		System.out.println(dan);
+		int bl = dan << 11;
+		System.out.println(bl);
+		
+	}
+	
+}
